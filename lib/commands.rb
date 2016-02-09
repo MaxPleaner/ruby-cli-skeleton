9faa5da70436a48ea45d2d3920edac2bc9e312d7
maxpleaner <maxpleaner@gmail.com>
2016-02-08 20:16:24 -0800

    more comment

--- lib/commands.rb
+++ lib/commands.rb
@@ -1,8 +1,10 @@
 class Commands
   def initialize(options={})
+    # make sure this method accepts a hash argument, which is passed from OptionParser
     puts "try 'help'".yellow
   end
   def hello_world(msg="")
+    # a sample event, call it with hello_world
     puts "hello_world #{msg}"
     return { foo: :bar }
   end
